70467fe10a4bc19aeef83d80e54110e5f951fb07
ivan mufid <ivanmohammad2355@gmail.com>
2022-03-18 16:18:14 +0700

    Sect12

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <ol>
+            <li v-for="todo in list" :key="todo.id">
+              {{ todo }} &nbsp;
+            </li>
+          </ol>
+      <p>
+        <input type="text" v-model="inputTodo" />
+        <button @click="create">Tambahkan</button>
+      </p>
+      <p v-if="list.length >= 4" > Hebat </p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App', 
+  data(){
+    return{
+      list: [],
+      inputTodo: ""
+    };
+  },
+  methods: {
+    create(){
+      if (this.inputTodo != ""){
+        this.list.push(this.inputTodo);
+        this.inputTodo = "";
+      }
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
